114fa8fc9d809a91527ac82a9e69c4eb7dbfd644
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-03-05 19:16:20 -0300

    projeto finalizado e funcional, porém os estilos não estão sendo aplicados

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Form from "./components/Form"
+import Form from "./components/Form";
+import './index.css';
 
 function App() {
 
   return (
-    <div>
+    <div className="bg-zinc-100 min-h-screen w-full flex flex-col items-center gap-6 pt-8">
       <h1 className="font-bold text-lxl">Formulário</h1>
       <Form />
     </div>
---
+++ src/hooks/useForms.tsx
@@ -0,0 +1,12 @@
+import { useForm } from "react-hook-form";
+import { formSchema, FormSchema } from "../schemas/FormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const useForms = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormSchema>({
+        resolver: zodResolver(formSchema)
+    })
+
+    return { register, handleSubmit, errors };
+}
