4b1f611f98bfe6f0190fe1b2b60c8cf3de3c7888
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-03-05 12:58:57 -0300

    setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from "./components/Form"
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="font-bold text-lxl">Formulário</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App;
